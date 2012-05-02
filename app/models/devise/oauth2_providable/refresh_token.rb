5b1f7b3b657f7c1e97693df7bed5f44b78c4a019
Billy Fowks <fowkswe@Williams-iMac.local>
2012-05-02 13:39:42 -0400

    attr_accessible for user, client on refresh_tokens

--- app/models/devise/oauth2_providable/refresh_token.rb
+++ app/models/devise/oauth2_providable/refresh_token.rb
@@ -4,6 +4,8 @@ class Devise::Oauth2Providable::RefreshToken
   include Devise::Oauth2Providable::ExpirableToken
   store_in :refresh_tokens
 
+  attr_accessible :user, :client
+  
   expires_according_to :refresh_token_expires_in
 
   has_many :access_tokens, :class_name=> "Devise::Oauth2Providable::AccessToken" 
